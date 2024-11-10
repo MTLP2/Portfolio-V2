4c4a0af81534bb6fe3b89ca553f6ff040422f244
Mathéo Lopes <matheo.lps91@gmail.com>
2024-11-10 20:40:06 +0100

    Update videoSection.jsx: Add playsInline attribute for inline playback on mobile

--- src/components/Home/Section/videoSection.jsx
+++ src/components/Home/Section/videoSection.jsx
@@ -7,7 +7,8 @@ const VideoSection = () => {
                 style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
                 autoPlay 
                 loop 
-                muted
+                muted 
+                playsInline // Enables inline playback on mobile
             >
                 <source src="https://mathlpbucket.s3.eu-west-3.amazonaws.com/Mon+film+1.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
