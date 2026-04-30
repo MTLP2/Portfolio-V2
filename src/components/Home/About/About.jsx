36aaf45618bf02d945d2b40e16eec8e2f5f064a3
Mathéo Lopes <matheo.lps91@gmail.com>
2026-04-30 20:20:02 +0200

    Refactor About component for improved readability; adjusted text formatting and layout for better presentation.

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -17,7 +17,9 @@ const About = () => {
       <div className="w-full flex gap-6 flex-col lg:flex-row items-center justify-center">
         <div className="flex flex-col gap-4 md:w-[45%] xl:w-[45%]">
           <div className="flex items-center gap-4">
-            <h2 className="text-2xl font-Tropical font-black dark:text-gray-100">Qui suis-je ? </h2>
+            <h2 className="text-2xl font-Tropical font-black dark:text-gray-100">
+              Qui suis-je ?{" "}
+            </h2>
             <hr className="border-black w-28 md:w-80 dark:border-gray-100" />
           </div>
           <p className="text-lg leading-6 font-sans font-extralight lg:w-[90%] text-[#2c0f0f]  dark:text-gray-100">
@@ -26,16 +28,19 @@ const About = () => {
             <br />
             {t("Line2")} <br />
             <br />
-            {t("Line3")}<br />
+            {t("Line3")}
+            <br />
             <br />
             {t("Line4")} <br />
             <br />
             {t("Line5")} <br />
             <br />
             <br />
-            {t("Line6")}<br />
+            {t("Line6")}
             <br />
-            {t("Line7")}<br />
+            <br />
+            {t("Line7")}
+            <br />
             <br />
             <br />
           </p>
@@ -57,13 +62,15 @@ const About = () => {
             onHoverEnd={() =>
               hrControls.start({ x: 98, y: 20, transition: { duration: 0.5 } })
             }
-            className=" w-full md:w-[400px] lg:w-full xl:w-[500px]  md:grayscale hover:grayscale-0 rounded-xl shadow-xl hover:shadow-2xl transition-shadow h-[500px] z-20"
+            className=" w-full object-cover md:w-[400px] lg:w-full xl:w-[500px]  md:grayscale hover:grayscale-0 rounded-xl shadow-xl hover:shadow-2xl transition-shadow h-[500px] z-20"
           />
         </div>
       </div>
       <div>
         <ul className="flex flex-wrap gap-4 w-[50%] mb-32">
-          <li className="bg-gray-200 p-2 shadow-md rounded-md">HTML & (S)CSS</li>
+          <li className="bg-gray-200 p-2 shadow-md rounded-md">
+            HTML & (S)CSS
+          </li>
           <li className="bg-gray-200 p-2 shadow-md rounded-md">TypeScript</li>
           <li className="bg-gray-200 p-2 shadow-md rounded-md">Vue.js</li>
           <li className="bg-gray-200 p-2 shadow-md rounded-md">React.js</li>
